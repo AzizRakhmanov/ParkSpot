0898d0e16d5eeb25e4431e32075cbb3cc054e458
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2024-09-11 16:45:40 +0500

    Add project files.

---
+++ ParkSpot/Areas/Identity/Data/ParkSpotDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ParkSpot.Areas.Identity.Data;
+
+namespace ParkSpot.Data;
+
+public class ParkSpotDbContext : IdentityDbContext<UserLoginModel>
+{
+    public ParkSpotDbContext(DbContextOptions<ParkSpotDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ ParkSpot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkSpot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
