42f778e72602948d5f5ed4f7e3938f421df314b7
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2024-09-15 00:33:26 +0500

    ViewModel added to models folder and one model for creation also written

---
+++ ParkSpot/Models/ViewModels/UserForCreateViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkSpot.Models.ViewModels
+{
+    public class UserForCreateViewModel
+    {
+        [Required]
+        [MinLength(2),MaxLength(100)]
+        public string FirstName {  get; set; }
+
+
+        [Required]
+        [MinLength(2), MaxLength(100)]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [MinLength(2), MaxLength(100)]
+        public string MiddleName { get; set; }
+
+
+        [Required]
+        [MinLength(2), MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
